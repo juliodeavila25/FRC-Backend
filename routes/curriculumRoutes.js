5d3b54be9408d00bd541fe905cd3bda5d696396c
Apuello1990 <apuello.aac@gmail.com>
2023-02-21 10:16:45 -0500

    update back

--- routes/curriculumRoutes.js
+++ routes/curriculumRoutes.js
@@ -6,14 +6,33 @@ import {
 
 import checkAuth from "../middleware/checkAuth.js";
 import express from "express";
+import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
-router.route("/").post(checkAuth, nuevoCurriculum);
+router
+  .route("/")
+  .post(
+    checkAuth,
+    upload.fields([
+      { name: "soporteExp" },
+      { name: "soporteEps" },
+      { name: "soportePension" },
+    ]),
+    nuevoCurriculum
+  );
 
 router
   .route("/:id")
   .get(checkAuth, obtenerCurriculum)
-  .put(checkAuth, editarCurriculum);
+  .put(
+    checkAuth,
+    upload.fields([
+      { name: "soporteExp" },
+      { name: "soporteEps" },
+      { name: "soportePension" },
+    ]),
+    editarCurriculum
+  );
 
 export default router;
